7fe551517e3af0f8a63581343d93b99a9d6ed1b5
hehql <374655767@qq.com>
2017-08-14 21:49:50 +0800

    DailyUpdate

---
+++ text.cpp
@@ -0,0 +1,34 @@
+/*************************************************************************
+	> File Name: text.cpp
+	> Author: heheql
+	> Mail: 374655767@qq.com
+	> Created Time: 2017年08月14日 星期一 00时28分06秒
+ ************************************************************************/
+
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+using namespace std;
+int fun()
+{
+    int a = 0;
+}
+int b = 0;
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    ios::sync_with_stdio(false);
+    int a = 1;
+    int a, b, c;
+    int aaa;
+
+    //  cin>>a;
+    if(a) {
+        cout << a << endl;
+        int a;
+    } else {
+        cout << 11 << endl;
+    }
+
+    return 0;
+}
